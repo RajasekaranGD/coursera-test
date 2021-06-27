4ecdf68dbfe1bb4e379100d4ff46c480b5854c8e
unknown <rajasekarangd116@gmail.com>
2021-06-27 10:39:33 +0530

    hiii

--- IOT1/js/script.js
+++ IOT1/js/script.js
@@ -1,12 +1,13 @@
 // Event handling
 $(document).ready(function(){
-    $.get("https://api.thingspeak.com/channels/1093334/feeds/last.json", 
+    $.get("https://api.thingspeak.com/channels/1093334/feeds.json?api_key=ZMT92WIXYMYJP6AW&results=1", 
       function(data, status){
       alert("Connection Status: " + status);
-      var message = "LAST UPDATED AT : " + data.created_at + 
-                    " CYLINDER LEVEL : " + data.field1 + "%";
+      console.log(data);
+      var message = "LAST UPDATED AT : " + data.feeds[0].created_at + 
+                    " CYLINDER LEVEL : " + data.feeds[0].field1 + "%";
       $('#content').html("<h2>" + message + "</h2>" );
-      if ( data.field1<10 ){
+      if ( data.feeds[0].field1<10 ){
         alert("Your GAS level is low..")
       }
   });
